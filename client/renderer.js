ac1bbc38555eddd6fdfb9a6f8d4e68ae17db0326
Michael Probber <git@mikep.ro>
2021-05-19 19:51:14 -0700

    Initial bundling

---
+++ client/renderer.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+import Hydra from 'hydra-synth';
+
+document.title = 'All hail hydra';
+
+document.body.style.margin = 0;
+
+const hydra = new Hydra();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-export const sum = (a: number, b: number): number => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { resolve } from 'path';
+
+import express from 'express';
+
+const app = express();
+
+const file = process.argv[process.argv.length - 1];
+
+const compiler = webpack({
+  mode: 'development',
+
+  plugins: [new HtmlWebpackPlugin()],
+  entry: [
+    resolve(__dirname, '..', 'client', 'renderer.js'),
+    resolve(process.cwd(), file),
+  ],
+});
+
+app.use(webpackDevMiddleware(compiler));
+
+app.listen(3000, () => {
+  console.log('app started');
+});
